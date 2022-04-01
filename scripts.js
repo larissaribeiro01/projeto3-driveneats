ac149eca981908227b1b0e3f978e5cad356af6b0
larissa <larissaribeabreu@gmail.com>
2022-03-31 21:53:55 -0300

    layout finalizado

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function EscolherPrato(prato) {
+    const pratoClicado=document.querySelector("."+ prato);
+    const pratoSelecionado=document.querySelector(".selecionado");
+    if (pratoSelecionado !== null) {
+        pratoSelecionado.classList.remove('selecionado')
+    }
+    pratoClicado.classList.add("selecionado")
+}
+
+
+function EscolherBebida(bebida) {
+    const bebidaClicado=document.querySelector("."+ bebida);
+    const bebidaSelecionado=document.querySelector(".selecionado");
+    if (bebidaSelecionado !== null) {
+        bebidaSelecionado.classList.remove('selecionado')
+    }
+    bebidaClicado.classList.add("selecionado")
+}
+
+
+function EscolherSobremesa(sobremesa) {
+    const sobremesaClicado=document.querySelector("."+ sobremesa);
+    const sobremesaSelecionado=document.querySelector(".selecionado");
+    if (sobremesaSelecionado !== null) {
+        sobremesaSelecionado.classList.remove('selecionado')
+    }
+    sobremesaClicado.classList.add("selecionado")
+}
+
