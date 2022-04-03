3757074bff82fbbec9b50f00d3b1a458ef83e294
larissa <larissaribeabreu@gmail.com>
2022-04-03 14:35:15 -0300

    centralização do Finalizar Pedido

--- scripts.js
+++ scripts.js
@@ -57,7 +57,7 @@ function ClicarFinalizar () {
     precoSobremesa=document.querySelector("."+sobremesaEscolhida).querySelector(".preco").innerHTML;
 
     let mensagem = "Olá, gostaria de fazer o pedido: \n" +prato+": " +precoPrato+ "\n" +bebida+": " +precoBebida+ "\n" +sobremesa+": " +precoSobremesa+ "\n" 
-    alert(mensagem)
+    
     if (finalizar == true) {
         window.open("https://wa.me/5521999999999?text="+mensagem);
     }
