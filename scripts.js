52323d10b5591bdd94325104e5d0f11e466c18b0
larissa <larissaribeabreu@gmail.com>
2022-04-03 14:26:14 -0300

    Finalizar Pedido com encaminhamento para o whatsapp

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+let pratoEscolhido;
+let bebidaEscolhida;
+let sobremesaEscolhida;
+
+
 function EscolherPrato(prato) {
     const pratoClicado=document.querySelector("."+ prato);
     const pratoSelecionado=document.querySelector(".pratos").querySelector(".selecionado");
@@ -5,6 +10,8 @@ function EscolherPrato(prato) {
         pratoSelecionado.classList.remove('selecionado')
     }
     pratoClicado.classList.add("selecionado")
+    pratoEscolhido=prato
+    FinalizarPed();
 }
 
 
@@ -15,6 +22,8 @@ function EscolherBebida(bebida) {
         bebidaSelecionado.classList.remove('selecionado')
     }
     bebidaClicado.classList.add("selecionado")
+    bebidaEscolhida=bebida
+    FinalizarPed();
 }
 
 
@@ -25,5 +34,31 @@ function EscolherSobremesa(sobremesa) {
         sobremesaSelecionado.classList.remove('selecionado')
     }
     sobremesaClicado.classList.add("selecionado")
+    sobremesaEscolhida=sobremesa
+    FinalizarPed();
 }
 
+
+function FinalizarPed () {
+    if (pratoEscolhido !== undefined && bebidaEscolhida !==undefined && sobremesaEscolhida !==undefined) {
+        document.querySelector(".botão").classList.add('finalizarpedido')
+        document.querySelector(".botão").innerHTML = "Finalizar Pedido"   
+    }
+
+}
+
+function ClicarFinalizar () {
+    finalizar=document.querySelector(".botão").classList.contains("finalizarpedido")
+    prato=document.querySelector("."+pratoEscolhido).querySelector(".Nome").innerHTML;
+    bebida=document.querySelector("."+bebidaEscolhida).querySelector(".Nome").innerHTML;
+    sobremesa=document.querySelector("."+sobremesaEscolhida).querySelector(".Nome").innerHTML;
+    precoPrato=document.querySelector("."+pratoEscolhido).querySelector(".preco").innerHTML;
+    precoBebida=document.querySelector("."+bebidaEscolhida).querySelector(".preco").innerHTML;
+    precoSobremesa=document.querySelector("."+sobremesaEscolhida).querySelector(".preco").innerHTML;
+
+    let mensagem = "Olá, gostaria de fazer o pedido: \n" +prato+": " +precoPrato+ "\n" +bebida+": " +precoBebida+ "\n" +sobremesa+": " +precoSobremesa+ "\n" 
+    alert(mensagem)
+    if (finalizar == true) {
+        window.open("https://wa.me/5521999999999?text="+mensagem);
+    }
+}
